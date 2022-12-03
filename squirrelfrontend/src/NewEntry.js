dfbc4888029e4886b265ad8d3a33e17f78356ef1
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-12-03 17:09:24 -0500

    Add event handling to form elements

--- squirrelfrontend/src/NewEntry.js
+++ squirrelfrontend/src/NewEntry.js
@@ -14,9 +14,33 @@ import { Button } from "@mui/material";
 
 
 export default function NewEntry(){
+    const[id, setId] = React.useState('');
+    const[color, setColor] = React.useState('black');
+    const[secondary, setSecondary] = React.useState('');
+    const[activity, setActivity] = React.useState('');
+    const[behavior, setBehavior] = React.useState('');
+    //const[error, setError] = React.useState(true);
+
+    const handleTextChange = (event) => {
+        setId(event.target.value);
+    }
+
+    const handleColorChange = (event) => {
+        setColor(event.target.value);
+        //setError(false);
+    }
     
-    function handleSubmit(){
-        console.log("submitted");
+    const handleActivityChange = (event) => {
+        setActivity(event.target.value);
+    }
+
+    const handleBehaviorChange = (event) => {
+        setBehavior(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        console.log("Submitted");
+        
     }
 
     return(
@@ -28,87 +52,94 @@ export default function NewEntry(){
             <Paper elevation={3} sx={{m:10, px:10, py:5}}>
                 <Typography variant="h6" >Enter Squirrel Information </Typography>
                 <br />
-                <FormControl sx={{width:'100%'}}>
-                    <Grid container spacing={2}>
-                        <Grid xs={6}>
-                        
-                            <TextField
-                                required
-                                id="sqId"
-                                label="Squirrel ID"
-                                defaultValue="" 
-                                sx={{width:200, mb: 4}}
-                            />
 
-                            <br/>
-
-                            <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
-                           
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="Black"
-                                name="radio-buttons-group"
-                            >
-                                <FormControlLabel value="black" control={<Radio />} label="Black" />
-                                <FormControlLabel value="gray" control={<Radio />} label="Gray" />
-                                <FormControlLabel value="cinnamon" control={<Radio />} label="Cinnamon" />
-
-                            </RadioGroup>
-
-                            <br/> <br/>
-
-                            <FormLabel id="demo-radio-buttons-group-label">Secondary Color</FormLabel>
-                           
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="Black"
-                                name="radio-buttons-group"
-                            >
-                                <FormControlLabel value="sblack" control={<Radio />} label="Black" />
-                                <FormControlLabel value="sgray" control={<Radio />} label="Gray" />
-                                <FormControlLabel value="scinnamon" control={<Radio />} label="Cinnamon" />
-
-                            </RadioGroup>
-
-                        </Grid>
-                        <Grid xs={6}>
-                            <FormLabel id="demo-radio-buttons-group-label">Activity</FormLabel>
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="Black"
-                                name="radio-buttons-group"
-                            >
-                                <FormControlLabel value="run" control={<Radio />} label="Running" />
-                                <FormControlLabel value="chase" control={<Radio />} label="Chasing" />
-                                <FormControlLabel value="climb" control={<Radio />} label="Climbing" />
-                                <FormControlLabel value="eat" control={<Radio />} label="Eating" />
-                            </RadioGroup>
+                <form onSubmit={handleSubmit}>
+                    <FormControl sx={{width:'100%'}}>
+                        <Grid container spacing={2}>
+                            <Grid xs={6}>
                             
-                            <br/>
+                                <TextField
+                                    required
+                                    id="sqId"
+                                    label="Squirrel ID"
+                                    defaultValue="" 
+                                    value={id}
+                                    onChange={handleTextChange}
+                                    sx={{width:200, mb: 4}}
+                                />
 
-                            <FormLabel id="demo-radio-buttons-group-label">Behavior</FormLabel>
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="Black"
-                                name="radio-buttons-group"
-                            >
-                                <FormControlLabel value="kuks" control={<Radio />} label="Kuks" />
-                                <FormControlLabel value="quaas" control={<Radio />} label="Quaas" />
-                                <FormControlLabel value="moans" control={<Radio />} label="Moans" />
-                                <FormControlLabel value="tflags" control={<Radio />} label="Tail Flags" />
-                                <FormControlLabel value="ttwitch" control={<Radio />} label="Tail Twitches" />
-                            </RadioGroup>
+                                <br/>
 
+                                <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
+                            
+                                <RadioGroup
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="radio-buttons-group"
+                                    value={color}
+                                    onChange={handleColorChange}
+                                >
+                                    <FormControlLabel value="black" control={<Radio />} label="Black" />
+                                    <FormControlLabel value="gray" control={<Radio />} label="Gray" />
+                                    <FormControlLabel value="cinnamon" control={<Radio />} label="Cinnamon" />
+
+                                </RadioGroup>
+
+                                <br/> <br/>
+
+                                <FormLabel id="demo-radio-buttons-group-label">Secondary Color</FormLabel>
+                            
+                                <RadioGroup
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="radio-buttons-group"
+                                >
+                                    <FormControlLabel value="sblack" control={<Radio />} label="Black" />
+                                    <FormControlLabel value="sgray" control={<Radio />} label="Gray" />
+                                    <FormControlLabel value="scinnamon" control={<Radio />} label="Cinnamon" />
+
+                                </RadioGroup>
+
+                            </Grid>
+                            <Grid xs={6}>
+                                <FormLabel id="demo-radio-buttons-group-label">Activity</FormLabel>
+                                <RadioGroup
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="radio-buttons-group"
+                                    value={activity}
+                                    onChange={handleActivityChange}
+                                >
+                                    <FormControlLabel value="run" control={<Radio />} label="Running" />
+                                    <FormControlLabel value="chase" control={<Radio />} label="Chasing" />
+                                    <FormControlLabel value="climb" control={<Radio />} label="Climbing" />
+                                    <FormControlLabel value="eat" control={<Radio />} label="Eating" />
+                                </RadioGroup>
+                                
+                                <br/>
+
+                                <FormLabel id="demo-radio-buttons-group-label">Behavior</FormLabel>
+                                <RadioGroup
+                                    aria-labelledby="demo-radio-buttons-group-label"
+                                    name="radio-buttons-group"
+                                    value={behavior}
+                                    onChange={handleBehaviorChange}
+                                >
+                                    <FormControlLabel value="kuks" control={<Radio />} label="Kuks" />
+                                    <FormControlLabel value="quaas" control={<Radio />} label="Quaas" />
+                                    <FormControlLabel value="moans" control={<Radio />} label="Moans" />
+                                    <FormControlLabel value="tflags" control={<Radio />} label="Tail Flags" />
+                                    <FormControlLabel value="ttwitch" control={<Radio />} label="Tail Twitches" />
+                                </RadioGroup>
+
+                            </Grid>
+                            
+                            <Grid xs={6} ></Grid>
+                            <Grid xs={2}>
+                                <Button variant="contained" sx={{marginTop:2}} type="submit" >
+                                    Submit
+                                </Button>
+                            </Grid>
                         </Grid>
-                        
-                        <Grid xs={6} ></Grid>
-                        <Grid xs={2}>
-                            <Button variant="contained" sx={{marginTop:2}} onclick={handleSubmit()}>
-                                Submit
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </FormControl>
+                    </FormControl>
+                </form>
 
             </Paper>
 
